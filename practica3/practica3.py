b9bc145a5d3a02d45fa8e50005b70dfc5cd72b29
Adrián Lattes <adrianlattes@disroot.org>
2022-03-02 19:55:15 +0100

    Apartado 2 y mejorando el codigo

--- practica3/practica3.py
+++ practica3/practica3.py
@@ -29,35 +29,44 @@ def kmeans_silhouettes(X, ns):
 def kmeans_elegir_n_clusters(ns, silhouettes, plot = True):
     silhouettes_max_index = silhouettes.index(max(silhouettes))
     n_clusters = ns[silhouettes_max_index]
-    plt.title("Valores medios del coeficiente de Silhouette para distintos números de vecindades", fontweight="bold")
-    plt.xlabel("Número de vecindades")
-    plt.ylabel("Valor medio del coeficiente de Silhouette")
-    bars = plt.bar(ns, silhouettes)
-    bars[silhouettes_max_index].set_color('r')
-    plt.xticks(ns)
-    # plt.xticklabels(range_n)
+    if plot:
+        plt.title("Valores medios del coeficiente de Silhouette para distintos números de vecindades", fontweight="bold")
+        plt.xlabel("Número de vecindades")
+        plt.ylabel("Valor medio del coeficiente de Silhouette")
+        bars = plt.bar(ns, silhouettes)
+        bars[silhouettes_max_index].set_color('r')
+        plt.xticks(ns)
     return n_clusters, silhouettes_max_index
 
 def plot_clusters_voronoi(X, clusters, n_clusters, ax = None):
     if ax is None:
         _, ax = plt.subplots()
     labels = clusters.labels_
-    unique_labels = set(labels)
-    colors = [cm.nipy_spectral(i/n_clusters) for i in range(len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
+    ax.scatter(
+            X[:, 0],
+            X[:, 1],
+            marker=".",
+            s=30,
+            lw=0,
+            alpha=0.7,
+            c=labels.astype(float),
+            edgecolor="k"
+    )
+    centers = clusters.cluster_centers_
+    ax.scatter(
+        centers[:, 0],
+        centers[:, 1],
+        marker="o",
+        c="white",
+        s=200,
+        edgecolor="k",
+    )
+    for i, c in enumerate(centers):
+        ax.scatter(c[0], c[1], marker=f"${i}$", alpha=1, s=50, edgecolor="k")
 
-        class_member_mask = (labels == k)
-
-        xy = X[class_member_mask]
-        ax.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=5)
     lims = (ax.get_xlim(), ax.get_ylim())
-
     # Diagrama de voronoi construido a partir de los centros de las vecindades
-    vor = Voronoi(clusters.cluster_centers_)
+    vor = Voronoi(centers)
     voronoi_plot_2d(vor, show_vertices = False, point_size=0, ax=ax)
 
     # Hack para que voronoi_plot_2d no cambie la escala de la gráfica.
@@ -81,41 +90,16 @@ def main():
     clusters = clusters[n_clusters_index]
 
     # Gráfica de la clasificación, con colores y líneas de separación del diagrama de Voronoi
-    plot_clusters_voronoi(X, clusters, n_clusters)
+    _, ax = plt.subplots()
+    plot_clusters_voronoi(X, clusters, n_clusters, ax)
+
+    test_data = [[0,0], [0, -1]]
+    for x, label in zip(test_data, clusters.predict(test_data)):
+        ax.scatter(x[0], x[1], alpha=1, s=20, color="r")
+        print(x, label)
 
     plt.show()
 
 
 if __name__ == "__main__":
     main()
-
-sys.exit(1)
-
-plt.subplots(1, 2)
-
-
-
-
-clusters = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
-labels = clusters.labels_
-unique_labels = set(labels)
-centers = clusters.cluster_centers_
-
-vor = Voronoi(centers)
-fig = voronoi_plot_2d(vor, show_vertices = False, point_size=0)
-
-colors = [cm.nipy_spectral(i / n_clusters) for i in range(len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-
-    class_member_mask = (labels == k)
-
-    xy = X[class_member_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=5)
-    p2.plot(fig)
-
-
-plt.show()
